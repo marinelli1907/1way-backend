e4b8a78cabc6f6f0ea07fc7eec509a6f39948d55
mohsinali02,m <mohsinalictn@gmail.com>
2025-09-16 16:14:50 +0500

    minor changes

--- Modules/UserManagement/Entities/Waitlist.php
+++ Modules/UserManagement/Entities/Waitlist.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Waitlist extends Model
 {
-    protected $table = 'waitlists';
+    protected $table = 'waitlist';
 
     protected $fillable = [
         'fullName',
